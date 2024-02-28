3ed09f1f1517cac411b942319b3e6e1ed367a421
1ronlion <leonardo.bana89@gmail.com>
2024-02-28 19:35:40 -0300

    afterclass

--- clase10/src/app.js
+++ clase10/src/app.js
@@ -15,20 +15,30 @@ app.use(express.json())
 app.use(express.static(__dirname+'/public'))
 app.engine('handlebars', handlebars.engine())
 
-app.use(viewRouter)
+//app.use(viewRouter)
+
+app.get('/',(req,res)=>{
+    res.render('home', {logs})
+})
 
 const server = app.listen(port,()=>console.log("Listening in", port))
 const io = new Server(server)//instanciando socket.io
 
+
+const logs = []
 
 io.on('connection', socket =>{
 
     console.log("Connected!")
 
     socket.on("message", (data)=> {
-        console.log(data)
-        //io.emit('log', data)
+    console.log("🚀 ~ socket.on ~ data:", data)
 
+    logs.push({socketid: socket.id, message: data})
+        
+        io.emit('log',{logs})
+        console.log("🚀 ~ logs:", logs)
+        
     })
 
 })
--- clase10/src/public/js/home.js
+++ clase10/src/public/js/home.js
@@ -1,10 +1,35 @@
 const socket = io()
+console.log("🚀 ~ ", "Connected!")
 
 //Primera parte: enviar caracter por caracter.
 const input = document.getElementById('textbox');
 const log = document.getElementById('log')
 input.addEventListener('keyup',evt=>{
-    let {key} = evt;
-    evt.target.value='';
-    console.log("🚀 ~ key:", key)
+    if(evt.key === "Enter"){
+        socket.emit('message', input.value)
+        //input.value = ""
+    }
 })
+
+socket.on('log', data => {
+
+    let logs = ""
+
+    data.logs.forEach(element => {
+        logs += `${element.socketid} = ${element.message}`
+    });
+
+    log.innerHTML+=logs;
+
+})
+
+
+//Desafio entregable
+// const form = document.getElementById("products")
+
+// form.addEventListener('submit', (e)=>{
+
+//     e.preventDefault()
+//     socket.emit('message', input.value)
+
+// })
