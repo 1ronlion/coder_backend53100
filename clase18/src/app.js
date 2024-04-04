944f9b99fc80b60c9f67e6aaecd74d1de7d86c98
Daniel Riverol <dannyriverol@hotmail.com>
2024-04-04 20:13:13 -0300

    Clase 18

--- clase18/src/app.js
+++ clase18/src/app.js
@@ -3,6 +3,7 @@ import __dirname from "./utils.js";
 import handlebars from "express-handlebars";
 import { Server } from "socket.io";
 import cookieParser from "cookie-parser";
+import session from "express-session";
 
 const app = express();
 const PORT = process.env.PORT || 4001;
@@ -17,6 +18,21 @@ app.use(express.json());
 app.use(express.static(__dirname + "/public"));
 app.engine("handlebars", handlebars.engine());
 app.use(cookieParser("hola")); //pasamos una clave
+app.use(
+  session({
+    secret: "Secreto",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+//middleware de auth
+function auth(req, res, next) {
+  if (req.session.user === "pepe" && req.session.admin) {
+    return next();
+  }
+  res.status(401).send("No estas autorizado");
+}
 
 //Routes
 app.get("/", (req, res) => {
@@ -24,9 +40,7 @@ app.get("/", (req, res) => {
 });
 //rutas cookies
 app.get("/setcookie", (req, res) => {
-  res
-    .cookie("micookie", "Soy el rey del mundo")
-    .send("Set cookie");
+  res.cookie("micookie", "Soy el rey del mundo").send("Set cookie");
 });
 app.get("/getcookie", (req, res) => {
   res.send(req.cookies);
@@ -46,7 +60,44 @@ app.get("/set-signed-cookie", (req, res) => {
 app.get("/get-signed-cookie", (req, res) => {
   res.send(req.signedCookies);
 });
+//ruta session
+app.get("/session", (req, res) => {
+  if (req.session.counter) {
+    req.session.counter++;
+    res.send("Se visitó el sitio " + req.session.counter + " veces");
+  } else {
+    req.session.counter = 1;
+    res.send("Bienvenido/a");
+  }
+});
+//iniciar sesion
 
+app.get("/login", (req, res) => {
+  const { username, password } = req.query;
+  if (username !== "pepe" || password !== "pepepass") {
+    return res.send("Login failed");
+  }
+
+  req.session.user = username;
+  req.session.admin = true;
+  res.send("Login Success");
+});
+
+//cerrar sesion
+app.get("/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (!err) {
+      res.send("Saliste de la sesion");
+    } else {
+      res.send({ error: err });
+    }
+  });
+});
+
+//rutas protegidas
+app.get('/privado',auth, (req,res)=>{
+  res.send("Estas en el mejor lugar")
+})
 //Listeners
 const server = app.listen(PORT, () =>
   console.log("Server listening on port", PORT)
