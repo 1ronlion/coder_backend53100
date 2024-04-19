d8f8c4c6d6c19623617d43568e669fee353192fd
Daniel Riverol <dannyriverol@hotmail.com>
2024-04-18 21:09:49 -0300

    Clase 23

--- clase23/customRouter/app.js
+++ clase23/customRouter/app.js
@@ -2,8 +2,12 @@ import express from "express";
 import UsersRouter from "./routes/users.js";
 
 const app = express();
-const port = 8080;
+const port = 4001;
 //rutas
+const usersRouter= new UsersRouter()
+
+app.use('/',usersRouter.getRouter())
+
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
